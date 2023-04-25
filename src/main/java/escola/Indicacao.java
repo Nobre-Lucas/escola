5c5709d47b2cfa3b006ad80c6618bdcc82e25282
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-04-25 10:14:07 -0300

    Add Indicacao Class

---
+++ src/main/java/escola/Indicacao.java
@@ -0,0 +1,13 @@
+package escola;
+
+public class Indicacao {
+
+	private Aluno indicante;
+	private Aluno indicado;
+
+	public Indicacao(Aluno indicante, Aluno indicado) {
+		this.indicante = indicante;
+		this.indicado = indicado;
+	}
+
+}
