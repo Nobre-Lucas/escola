d3d01ec0cb4367833f76e83388185884f99f34fe
Lucas Nobre Barbosa <lucasbnobre18@gmail.com>
2023-04-25 10:17:35 -0300

    Add dataDaIndicacao atribute

--- src/main/java/escola/Indicacao.java
+++ src/main/java/escola/Indicacao.java
@@ -1,13 +1,17 @@
 package escola;
 
+import java.time.LocalDateTime;
+
 public class Indicacao {
 
 	private Aluno indicante;
 	private Aluno indicado;
+	private LocalDateTime dataDaIndicacao;
 
 	public Indicacao(Aluno indicante, Aluno indicado) {
 		this.indicante = indicante;
 		this.indicado = indicado;
+		this.dataDaIndicacao = LocalDateTime.now();
 	}
 
 }
